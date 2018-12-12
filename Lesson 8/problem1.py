3c966ee959cc27b898d29e5316783c96288ff176
samantharina <43784570+samantharina@users.noreply.github.com>
2018-12-12 12:35:59 -0500

    Add files via upload

---
+++ Lesson 8/problem1.py
@@ -0,0 +1,32 @@
+print ('-' * 65)
+
+print('Morbid Predictions Program:')
+
+print( )
+
+
+#Description
+
+print('This program asks you for your current age and tells you the year that you will die (on average)')
+
+
+age = input('What is your age today?: ')
+age = int(age)
+
+
+print ('...thinking...')
+print ('...thinking...')
+print ('...thinking...')
+
+result = 2018 - age 
+answer = result + 100
+answer = str(answer)
+
+
+
+print ('You will die in the year ' + answer)
+result = int(answer)
+
+
+print ('-' * 65)
+
---
+++ Lesson 8/problem2.py
@@ -0,0 +1,27 @@
+message = 'tjwz wz tjq zqurqt pqzzagq'
+
+# do not change the code above this line
+
+# Write your algorithm to 
+
+
+message = message.replace('q' , 'e')
+message = message.replace('z' , 's')
+message = message.replace('p' , 'm')
+message = message.replace('u' , 'c')
+message = message.replace('w' , 'i')
+message = message.replace('j' , 'h')
+
+print(message)
+
+
+
+
+
+
+
+
+
+# do not change the code below this line
+
+print(message)
